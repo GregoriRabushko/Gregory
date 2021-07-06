8fc27d74f10a0a5a28c399555fe6a9a9c613fa6b
Gregory Rabushko <zak_zan@bk.ru>
2021-07-06 23:14:30 +0300

    решил первый урок и поправил код

--- script.js
+++ script.js
@@ -6,3 +6,17 @@
  *     Подсказка: вытащи их по id и сразу возьми значение поля value.
  * 2.1 Присвой в переменную calculationResult результат сложения этих двух переменных.
  */
+
+function sum() {
+ let calculationResult;
+const a = +document.getElementById('first').value;
+const b = +document.getElementById('second').value; 
+ calculationResult = a + b; 
+ return calculationResult
+}
+
+const setResult = function() {
+    document.getElementById('resultField').innerText = sum().toString();
+}
+
+document.getElementById('resultButton').onclick = setResult
