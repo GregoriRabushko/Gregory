65649e22d8e07b3c4ffac550c8ddeec76338b142
Dmitry Berestovsky <mtsby7049874@gmail.com>
2021-07-12 14:14:55 +0300

    fix: lesson 1
    Fix teacher's comment

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function sum() {
   let calculationResult;
 
-  // Лучше дать этим переменным более "говорящие" имена, помогающие определить их назначение в коде
+  // TODO: Лучше дать этим переменным более "говорящие" имена, помогающие определить их назначение в коде
   // Например, firstNumber и secondNumber
   const a = +document.getElementById("first").value;
   const b = +document.getElementById("second").value;
 
-  // Эти две строки можно сократить в одну, возвращая сразу результат выражения (return a + b).
+  // TODO: Эти две строки можно сократить в одну, возвращая сразу результат выражения (return a + b).
   // Тогда переменная calculationResult не понадобится.
   calculationResult = a + b;
   return calculationResult;
