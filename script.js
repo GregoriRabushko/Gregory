7f0d25b2ee034182ab298ec97040a38f6ab519ba
Gregory Rabushko <zak_zan@bk.ru>
2021-08-21 21:27:19 +0300

    исправление по совету

--- script.js
+++ script.js
@@ -9,17 +9,17 @@
  */
 
 function sum() {
-  let calculationResult;
+
 
   // TODO: Лучше дать этим переменным более "говорящие" имена, помогающие определить их назначение в коде
   // Например, firstNumber и secondNumber
-  const a = +document.getElementById("first").value;
-  const b = +document.getElementById("second").value;
+  const firstNumber = +document.getElementById("first").value;
+  const secondNumber = +document.getElementById("second").value;
 
   // TODO: Эти две строки можно сократить в одну, возвращая сразу результат выражения (return a + b).
   // Тогда переменная calculationResult не понадобится.
-  calculationResult = a + b;
-  return calculationResult;
+ 
+  return  firstNumber + secondNumber;
 }
 
 const setResult = function () {
