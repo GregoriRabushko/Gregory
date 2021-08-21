ce456d12b24db1e38f774a16ce645f033bb15987
Dmitry Berestovsky <mtsby7049874@gmail.com>
2021-08-21 21:55:23 +0300

    fix: lesson 2 previous HM
    apply changes from previous homework

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 function sum() {
-  let calculationResult;
+    const firstNumber = +document.getElementById("first").value;
+    const secondNumber = +document.getElementById("second").value;
 
-  // TODO: Лучше дать этим переменным более "говорящие" имена, помогающие определить их назначение в коде
-  // Например, firstNumber и secondNumber
-  const a = +document.getElementById("first").value;
-  const b = +document.getElementById("second").value;
-
-  // TODO: Эти две строки можно сократить в одну, возвращая сразу результат выражения (return a + b).
-  // Тогда переменная calculationResult не понадобится.
-  calculationResult = a + b;
-  return calculationResult;
+    return firstNumber + secondNumber;
 }
 
 const setResult = function () {
-  document.getElementById("resultField").innerText = sum().toString();
+    document.getElementById("resultField").innerText = sum().toString();
 };
 
 document.getElementById("resultButton").onclick = setResult;
