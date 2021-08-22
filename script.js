1b178a5b09ea0f938f8463faacf8c133fefde16f
Dmitry Berestovsky <mtsby7049874@gmail.com>
2021-08-22 20:18:23 +0300

    fix: lesson 3 refactoring of add history

--- script.js
+++ script.js
@@ -1,30 +1,77 @@
 const setResult = function (result = 0) {
-    document.getElementById("resultField").innerText = result.toString();
+    document.getElementById("resultField").innerText = result.toFixed(4);
 };
 
+function getFirstNumber() {
+    return +document.getElementById("first").value;
+}
+
+function getSecondNumber() {
+    return +document.getElementById("second").value;
+}
+
+function getNumbers() {
+    const firstNumber = getFirstNumber();
+    const secondNumber = getSecondNumber();
+
+    return {firstNumber, secondNumber}
+}
+
+const history = {
+    count: 0
+}
+
+function addRecordToHistory(first, second, result, operator) {
+    history.count++
+    const key = `operation_${history.count}`; // 'operation_' + history.count
+    history[key] = {
+        first, second, result, operator,
+        toString() {
+            return `${first} ${operator} ${second} = ${result}`
+        },
+        toHtmlListItem() {
+            return `<li>${this.first} ${this.operator} ${this.second} = ${this.result}</li>>`
+        },
+        toHtmlTableRow() {
+            return `<tr><td>${this.first}</td><td>${this.operator}</td><td>${this.second}</td><td>${this.result}</td></tr>>`
+        }
+    }
+}
+
 function sum() {
-    const firstNumber = +document.getElementById("first").value;
-    const secondNumber = +document.getElementById("second").value;
-
-    // const func = () => {
-    //     console.log(firstNumber)
-    // }
-    //
-    // func();
-
-    const result = firstNumber + secondNumber;
-
+    const {firstNumber: first, secondNumber: second} = getNumbers()
+    const result = first + second;
     setResult(result)
+    addRecordToHistory(first, second, result, '+')
 }
 
 function sub() {
-    const firstNumber = +document.getElementById("first").value;
-    const secondNumber = +document.getElementById("second").value;
-
+    const {firstNumber, secondNumber} = getNumbers()
     const result = firstNumber - secondNumber;
-
     setResult(result)
+    addRecordToHistory(firstNumber, secondNumber, result, '-')
+}
+
+function div() {
+    const {firstNumber, secondNumber} = getNumbers()
+    if (secondNumber === 0) {
+        setResult("");
+        alert("на ноль делить нельзя");
+    } else {
+        const result = firstNumber / secondNumber;
+        setResult(result)
+        addRecordToHistory(firstNumber, secondNumber, result, '/')
+    }
+}
+
+function mult() {
+    const {firstNumber, secondNumber} = getNumbers()
+    const result = firstNumber * secondNumber;
+    setResult(result)
+    addRecordToHistory(firstNumber, secondNumber, result, '*')
 }
 
 document.getElementById("sumButton").onclick = sum;
 document.getElementById("subButton").onclick = sub;
+document.getElementById("divButton").onclick = div;
+document.getElementById("multButton").onclick = mult;
--- tasks/lesson-003/task-003.md
+++ tasks/lesson-003/task-003.md
@@ -2,5 +2,20 @@
 ## Применение знаний об объектах, замыкании и импортах.
 
 ### Задание:
-- выделить функции арифметических операций в отдельный скрипт.
 - добавить объект, хранящий историю вычислений
+
+### Полезные ссылки:
+[Объекты](https://learn.javascript.ru/object) <br/>
+[Symbol](https://learn.javascript.ru/symbol) <br/>
+[Деструктурирующее присваивание](https://learn.javascript.ru/destructuring-assignment) <br/>
+[Преобразование объектов в примитивы](https://learn.javascript.ru/object-toprimitive) <br/>
+
+[Замыкания](https://learn.javascript.ru/closure) <br/>
+[Циклы while и for](https://learn.javascript.ru/while-for) <br/>
+[Конструкция "switch"](https://learn.javascript.ru/switch) <br/>
+[Модули](https://learn.javascript.ru/modules-intro) <br/>
+[Экспорт и импорт](https://learn.javascript.ru/import-export) <br/>
+[Динамические импорты](https://learn.javascript.ru/modules-dynamic-imports) <br/>
+[Webpack](https://webpack.js.org/guides/getting-started/) <br/>
+[Основные понятия в Webpack](https://webpack.js.org/concepts/) <br/>
+[Настройка Webpack](https://webpack.js.org/configuration/) <br/>
