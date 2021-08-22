d92779944b7da31a49be6468de5ab3f477100126
Gregory Rabushko <zak_zan@bk.ru>
2021-08-22 12:36:30 +0300

    добавления 2 функций

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
-const setResult = function (result = 0) {
-    document.getElementById("resultField").innerText = result.toString();
+
+  const firstNumber = +document.getElementById("first").value;
+  const secondNumber = +document.getElementById("second").value;
+
+
+  function sum() {
+    const result = firstNumber + secondNumber;
+ 
+    setResult(result)
+  };
+
+  
+  
+function sub() {
+  const result = firstNumber - secondNumber;
+
+  setResult(result)
 };
 
-function sum() {
-    const firstNumber = +document.getElementById("first").value;
-    const secondNumber = +document.getElementById("second").value;
 
-    // const func = () => {
-    //     console.log(firstNumber)
-    // }
-    //
-    // func();
 
-    const result = firstNumber + secondNumber;
 
-    setResult(result)
-}
+function div() {
+  const result = firstNumber / secondNumber;
+  
+  setResult(result)
+};
 
-function sub() {
-    const firstNumber = +document.getElementById("first").value;
-    const secondNumber = +document.getElementById("second").value;
 
-    const result = firstNumber - secondNumber;
 
-    setResult(result)
-}
+function mult() {
+  const result = firstNumber * secondNumber;
+  
+  setResult(result)
+};
 
 document.getElementById("sumButton").onclick = sum;
 document.getElementById("subButton").onclick = sub;
+document.getElementById("divButton").onclick = div;
+document.getElementById("multButton").onclick = mult;
+
+const setResult = function (result = 0) {
+    document.getElementById("resultField").innerText = result.toString();
+  };    
