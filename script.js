1172603a694214267d5f31486c298c8e2d9bdeec
Dmitry Berestovsky <mtsby7049874@gmail.com>
2021-10-17 00:20:26 +0300

    feat: history log

--- script.js
+++ script.js
@@ -32,42 +32,72 @@ function addRecordToHistory(first, second, result, operator) {
     history[key] = {
         first, second, result, operator,
         toString() {
-            return `${this.first} ${this.operator} ${this.second} = ${this.result}` // 10 + 15 = 25
+            return `${first} ${operator} ${second} = ${result}` // 10 + 15 = 25
         },
         toHtmlListItem() {
-            return `<li>${this.first} ${this.operator} ${this.second} = ${this.result}</li>>`
+            return `<li>${first} ${operator} ${second} = ${result}</li>>`
         },
         toHtmlTableRow() {
-            return `<tr><td>${this.first}</td><td>${this.operator}</td><td>${this.second}</td><td>${this.result}</td></tr>>`
+            return `<tr><td>${first}</td><td>${operator}</td><td>${second}</td><td>${result}</td></tr>>`
         }
     }
 
     /*
     history = {
-      count: 1,
+      count: 3,
       operation_1: {
         first: 10,
         second: 15,
         result: 25,
         operator: '+'
-      }
-
+      },
+      operation_2: {
+        first: 10,
+        second: 15,
+        result: 25,
+        operator: '+'
+      },
+      operation_3: {
+        first: 10,
+        second: 15,
+        result: 25,
+        operator: '+'
+      },
     }
      */
 }
 
+
+const logHistory = function () {
+    console.log('=====BEGIN HISTORY=====');
+    for (const key in history) {
+        if (key === 'count') {
+            continue;
+        }
+        console.log(`${key}: `, history[key])
+    } // ключи
+    console.log('=====END HISTORY=====');
+}
+
+// const c = [1, 2, 3, 4, 5];
+// for (let i = 0; i < c.length; i++) {
+//     console.log(c[i])
+// }
+
 function sum() {
     const {firstNumber: first, secondNumber: second} = getNumbers()
     const result = first + second;
-    setResult(result)
-    addRecordToHistory(first, second, result, '+')
+    setResult(result);
+    addRecordToHistory(first, second, result, '+');
+    logHistory();
 }
 
 function sub() {
     const {firstNumber, secondNumber} = getNumbers()
     const result = firstNumber - secondNumber;
-    setResult(result)
-    addRecordToHistory(firstNumber, secondNumber, result, '-')
+    setResult(result);
+    addRecordToHistory(firstNumber, secondNumber, result, '-');
+    logHistory();
 }
 
 function div() {
@@ -77,19 +107,33 @@ function div() {
         alert("на ноль делить нельзя");
     } else {
         const result = firstNumber / secondNumber;
-        setResult(result)
-        addRecordToHistory(firstNumber, secondNumber, result, '/')
+        setResult(result);
+        addRecordToHistory(firstNumber, secondNumber, result, '/');
+        logHistory();
     }
 }
 
 function mult() {
     const {firstNumber, secondNumber} = getNumbers()
     const result = firstNumber * secondNumber;
-    setResult(result)
-    addRecordToHistory(firstNumber, secondNumber, result, '*')
+    setResult(result);
+    addRecordToHistory(firstNumber, secondNumber, result, '*');
+    logHistory();
 }
 
 document.getElementById("sumButton").onclick = sum;
 document.getElementById("subButton").onclick = sub;
 document.getElementById("divButton").onclick = div;
 document.getElementById("multButton").onclick = mult;
+
+
+const a = {
+    x: {
+        y: 2, // 5
+        z: '3'
+    }
+}
+
+const c = {...a};
+
+c.x.y = 5
